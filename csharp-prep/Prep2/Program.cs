74e6ff2ecd1d5d5d368734ae2343aaa50af67da7
Nicholas Keen <nicknack2019@yahoo.com>
2024-04-27 21:00:53 -0600

    completed prep1 and prep2 assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int user_grade = int.Parse(grade);
+
+        string letter = "";
+
+        if (user_grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (user_grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (user_grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (user_grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (user_grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
+
+
